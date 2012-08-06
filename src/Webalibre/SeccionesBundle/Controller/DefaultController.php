ac4aec915601ff71b4119dfa13d81fe7d00c314b
juampa <jpasosa@gmail.com>
2012-08-06 19:14:31 -0300

    DefaultController actions de las secciones

--- src/Webalibre/SeccionesBundle/Controller/DefaultController.php
+++ src/Webalibre/SeccionesBundle/Controller/DefaultController.php
@@ -12,19 +12,31 @@ class DefaultController extends Controller
         return $this->render('WebalibreSeccionesBundle:Default:homepage.html.twig');
     }
 
-    public function homepageAction() {
-        return $this->render('WebalibreSeccionesBundle:Default:homepage.html.twig');
+    public function symfony2Action() {
+        return $this->render('WebalibreSeccionesBundle:Default:symfony2.html.twig');
     }
 
-    
+    public function linuxAction() {
+        return $this->render('WebalibreSeccionesBundle:Default:linux.html.twig');
+    }
 
+  public function jqueryAction() {
+        $acciones = $this->getAction();
+        // echo $acciones;
+        return $this->render('WebalibreSeccionesBundle:Default:jquery.html.twig');
+         }
 
+    public function otrosAction() {
+        return $this->render('WebalibreSeccionesBundle:Default:otros.html.twig');
+    }
 
+    public function portfolioAction() {
+        return $this->render('WebalibreSeccionesBundle:Default:portfolio.html.twig');
+    }
 
-
-
-
-
+    public function contactoAction() {
+        return $this->render('WebalibreSeccionesBundle:Default:contacto.html.twig');
+    }
 
 
 }
