ef1f997d9bf6e58627a7fc08d2c5b51752acc6b2
juampa <jpasosa@gmail.com>
2012-08-07 12:36:48 -0300

    homepage scroll ++

--- web/bundles/webalibresecciones/js/scroll-up.js
+++ web/bundles/webalibresecciones/js/scroll-up.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+        num = Math.random() * 10;
+        num = Math.round(num);
         $(window).scroll(function(){
+            texto = 'url("/bundles/webalibresecciones/images/base/scroll-up' + num + '.png") no-repeat';
             if ($(this).scrollTop() > 300) {
+                $('.scrollup').css('background', texto);
                 $('.scrollup').fadeIn();
             } else {
+                $('.scrollup').css('background', texto);
                 $('.scrollup').fadeOut();
             }
         });
