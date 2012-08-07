f66d3570837b2fc9bc73efdb091d4662114966c5
juampa <jpasosa@gmail.com>
2012-08-06 23:06:15 -0300

    menues activos

--- src/Webalibre/SeccionesBundle/Controller/DefaultController.php
+++ src/Webalibre/SeccionesBundle/Controller/DefaultController.php
@@ -8,34 +8,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
+    // Obtengo la accion leyendo la url, para poder usarla en el menú de navegación.
+    public function getActionName() {
+        $url = $_SERVER['REQUEST_URI'];
+        $url_exp = explode('/', $url);
+        $accion = end($url_exp);
+        return $accion;
+    }
+
     public function homepageAction() {
-        return $this->render('WebalibreSeccionesBundle:Default:homepage.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:homepage.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
     }
 
     public function symfony2Action() {
-        return $this->render('WebalibreSeccionesBundle:Default:symfony2.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:symfony2.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
     }
 
     public function linuxAction() {
-        return $this->render('WebalibreSeccionesBundle:Default:linux.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:linux.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
     }
 
   public function jqueryAction() {
-        $acciones = $this->getAction();
-        // echo $acciones;
-        return $this->render('WebalibreSeccionesBundle:Default:jquery.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:jquery.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
          }
 
     public function otrosAction() {
-        return $this->render('WebalibreSeccionesBundle:Default:otros.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:otros.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
     }
 
     public function portfolioAction() {
-        return $this->render('WebalibreSeccionesBundle:Default:portfolio.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:portfolio.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
     }
 
     public function contactoAction() {
-        return $this->render('WebalibreSeccionesBundle:Default:contacto.html.twig');
+        return $this->render('WebalibreSeccionesBundle:Default:contacto.html.twig', array(
+                                                                                            'actionname' => $this->getActionName()));
     }
 
 
