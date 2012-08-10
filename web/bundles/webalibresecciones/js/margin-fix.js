e9899038e4f6f8b12a32b74949b9ed0fd09cd586
juampa <jpasosa@gmail.com>
2012-08-10 10:56:48 -0300

    margin-fix(js) en el footer... v2.0(ver si salen errores x consola.) Funciona Perfecto

--- web/bundles/webalibresecciones/js/margin-fix.js
+++ web/bundles/webalibresecciones/js/margin-fix.js
@@ -1,22 +1,50 @@
 $(document).ready(function(){
     var widthdiv= $('.up').css('width');
-    var anchoTotal = parseInt(widthdiv);
-    // alert(anchoTotal + 1000);
-    var margen_img = $('.imagenes').css('width');
+    var widthTotal = parseInt(widthdiv);
+    // alert(widthTotal + 1000);
+    // var margen_img = $('.imagenes').css('width');
     // alert(margen_img);
     
-    var ancho_total = 0;
-    var cant_img = 0
+
+    // Realización de acomodación en la parte SUPERIOR del footer
+    var widthImgTotal = 0;      // ancho total de todas las imagenes del footer
+    var countIMG = 0;             // cantidad de imagenes del footer
     $('.up .footer-img').each(function(index) {
-        cant_img++;
+        countIMG++;
         var width = $(this).attr('width');
-        var width_footer_img = parseInt(width);
-        ancho_total = ancho_total + width_footer_img;
+        var widthIMG = parseInt(width);
+        widthImgTotal = widthImgTotal + widthIMG;
     });
-    var margen = ((( anchoTotal - ancho_total ) / cant_img) / 2) - 3;
-    // alert(margen);
-    $('.up .footer-img').css('margin-left', margen);
-    $('.up .footer-img').css('margin-right', margen);
-    // alert(totalancho);
-    // alert(suma_ancho_imagenes);
+    
+    // defino primeros y último margen de las imágenes
+    var firstMarginLeft = lastMarginRight = (( widthTotal - widthImgTotal ) / ( countIMG + 1 )) - 3;
+    var allMargin = firstMarginLeft / 2;  // defino los margin-right y margin-left de las imagenes
+
+    // acomodo los márgenes según los cálculos realizados
+    $('.up .footer-img').css('margin-left', allMargin);
+    $('.up .footer-img').css('margin-right', allMargin);
+    $('.up img.footer-img:first').css('margin-left', firstMarginLeft);
+    $('.up img:last').css('margin-right', lastMarginRight);
+
+    // Realización de acomodación en la parte INFERIOR del footer
+    var widthImgTotal = 0;      // ancho total de todas las imagenes del footer
+    var countIMG = 0;             // cantidad de imagenes del footer
+    $('.down .footer-img').each(function(index) {
+        countIMG++;
+        var width = $(this).attr('width');
+        var widthIMG = parseInt(width);
+        widthImgTotal = widthImgTotal + widthIMG;
+    });
+    
+    // defino primeros y último margen de las imágenes
+    var firstMarginLeft = lastMarginRight = (( widthTotal - widthImgTotal ) / ( countIMG + 1 )) - 3;
+    var allMargin = firstMarginLeft / 2;  // defino los margin-right y margin-left de las imagenes
+
+    // acomodo los márgenes según los cálculos realizados
+    $('.down .footer-img').css('margin-left', allMargin);
+    $('.down .footer-img').css('margin-right', allMargin);
+    $('.down img.footer-img:first').css('margin-left', firstMarginLeft);
+    $('.down img:last').css('margin-right', lastMarginRight);
+
+
 });
